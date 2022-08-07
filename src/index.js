ba474644e4473851c400503cc86a314fa1ee5010
Eric Lund <ericlund4@gmail.com>
2022-08-06 23:50:42 -0500

    Getting to Function Component stage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+//TODO at function component step
 class Square extends React.Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick({ value: "X" })}
+      >
+        {this.propss.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { squares: Array(9).fill(null) };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
